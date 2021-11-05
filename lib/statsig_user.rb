4d374978062400ee069fddf049c08b24d094442c
Jiakan Wang <jkw@statsig.io>
2021-11-05 16:45:51 -0700

    custom IDs

--- lib/statsig_user.rb
+++ lib/statsig_user.rb
@@ -7,6 +7,7 @@ class StatsigUser
   attr_accessor :locale
   attr_accessor :app_version
   attr_accessor :statsig_environment
+  attr_accessor :custom_ids
   attr_accessor :private_attributes
 
   def custom
@@ -28,9 +29,11 @@ class StatsigUser
       @country = user_hash['country']
       @locale = user_hash['locale']
       @app_version = user_hash['appVersion'] || user_hash['app_version']
-      @custom = user_hash['custom']
+      @custom = user_hash['custom'] if user_hash['custom'].is_a? Hash
       @statsig_environment = user_hash['statsigEnvironment']
-      @private_attributes = user_hash['privateAttributes']
+      @private_attributes = user_hash['privateAttributes'] if user_hash['privateAttributes'].is_a? Hash
+      custom_ids = user_hash['customIDs'] || user_hash['custom_ids']
+      @custom_ids = custom_ids if custom_ids.is_a? Hash
     end
   end
 
@@ -46,6 +49,7 @@ class StatsigUser
       'custom' => @custom,
       'statsigEnvironment' => @statsig_environment,
       'privateAttributes' => @private_attributes,
+      'customIDs' => @custom_ids,
     }
     if for_logging
       hash.delete('privateAttributes')
