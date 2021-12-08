91a690e2a5e21e29f9783a6fda649febc6e3bc19
Jiakan Wang <jkw@statsig.io>
2021-12-07 16:16:21 -0800

    add logic to parse id lists from config specs

--- lib/spec_store.rb
+++ lib/spec_store.rb
@@ -7,6 +7,7 @@ class SpecStore
     @store = {
       :gates => {},
       :configs => {},
+      :id_lists => {},
     }
     process(specs_json)
   end
@@ -21,13 +22,28 @@ class SpecStore
       !specs_json['feature_gates'].nil? &&
       !specs_json['dynamic_configs'].nil?
 
-    @store = {
-      :gates => {},
-      :configs => {},
-    }
+    new_gates = {}
+    new_configs = {}
 
-    specs_json['feature_gates'].map{|gate|  @store[:gates][gate['name']] = gate }
-    specs_json['dynamic_configs'].map{|config|  @store[:configs][config['name']] = config }
+    specs_json['feature_gates'].map{|gate|  new_gates[gate['name']] = gate }
+    specs_json['dynamic_configs'].map{|config|  new_configs[config['name']] = config }
+    @store[:gates] = new_gates
+    @store[:configs] = new_configs
+
+    new_id_lists = specs_json['id_lists']
+    if new_id_lists.is_a? Hash
+      new_id_lists.each do |list_name, _|
+        if !@store[:id_lists].key?(list_name)
+          @store[:id_lists][list_name] = {ids: {}, time: 0}
+        end
+      end
+
+      @store[:id_lists].each do |list_name, _|
+        if !new_id_lists.key?(list_name)
+          @store[:id_lists].delete(list_name)
+        end
+      end
+    end
   end
 
   def has_gate?(gate_name)
