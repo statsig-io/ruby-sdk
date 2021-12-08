22bd72086268548ec26924b145adb70ddf842c85
Jiakan Wang <jkw@statsig.io>
2021-12-07 17:55:43 -0800

    refactor store

--- lib/spec_store.rb
+++ lib/spec_store.rb
@@ -2,14 +2,64 @@ require 'net/http'
 require 'uri'
 
 class SpecStore
-  def initialize(specs_json)
+  def initialize(network, error_callback)
+    @network = network
     @last_sync_time = 0
     @store = {
       :gates => {},
       :configs => {},
       :id_lists => {},
     }
-    process(specs_json)
+    e = download_config_specs
+    error_callback.call(e) unless error_callback.nil?
+
+    @config_sync_thread = sync_config_specs
+  end
+
+  def shut_down
+    @config_sync_thread&.exit
+  end
+
+  def has_gate?(gate_name)
+    @store[:gates].key?(gate_name)
+  end
+
+  def has_config?(config_name)
+    @store[:configs].key?(config_name)
+  end
+
+  def get_gate(gate_name)
+    return nil unless has_gate?(gate_name)
+    @store[:gates][gate_name]
+  end
+
+  def get_config(config_name)
+    return nil unless has_config?(config_name)
+    @store[:configs][config_name]
+  end
+
+  private
+
+  def sync_config_specs
+    Thread.new do
+      loop do
+        sleep 10
+        download_config_specs
+      end
+    end
+  end
+
+  def download_config_specs
+    begin
+      response, e = @network.post_helper('download_config_specs', JSON.generate({'sinceTime' => @last_sync_time}))
+      if e.nil?
+        process(JSON.parse(response.body))
+      else
+        e
+      end
+    rescue StandardError => e
+      e
+    end
   end
 
   def process(specs_json)
@@ -33,35 +83,17 @@ class SpecStore
     new_id_lists = specs_json['id_lists']
     if new_id_lists.is_a? Hash
       new_id_lists.each do |list_name, _|
-        if !@store[:id_lists].key?(list_name)
-          @store[:id_lists][list_name] = {ids: {}, time: 0}
+        unless @store[:id_lists].key?(list_name)
+          @store[:id_lists][list_name] = { ids: {}, time: 0 }
         end
       end
 
       @store[:id_lists].each do |list_name, _|
-        if !new_id_lists.key?(list_name)
+        unless new_id_lists.key?(list_name)
           @store[:id_lists].delete(list_name)
         end
       end
     end
-  end
-
-  def has_gate?(gate_name)
-    return @store[:gates].key?(gate_name)
-  end
-
-  def has_config?(config_name)
-    return @store[:configs].key?(config_name)
-  end
-
-  def get_gate(gate_name)
-    return nil unless has_gate?(gate_name)
-    @store[:gates][gate_name]
-  end
-
-  def get_config(config_name)
-    return nil unless has_config?(config_name)
-    @store[:configs][config_name]
   end
 
 end
