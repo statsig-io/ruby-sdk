6adfcb1e2889971f24852860fefdf2bc0a60f27a
Kenny Yi <kenny@statsig.com>
2023-05-03 13:30:22 -0700

    switch to cobertura

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,15 @@
 
 require 'simplecov'
 require 'simplecov-lcov'
+require 'simplecov-cobertura'
+SimpleCov.formatter = if ENV['COVERAGE_FORMAT'] == 'cobertura'
+                        SimpleCov::Formatter::CoberturaFormatter
+                      elsif ENV['COVERAGE_FORMAT'] == 'lcov'
+                        SimpleCov::Formatter::LcovFormatter
+                      else
+                        SimpleCov::Formatter::HTMLFormatter
+                      end
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatter = ENV['LCOV'] ? SimpleCov::Formatter::LcovFormatter : SimpleCov::Formatter::HTMLFormatter
 SimpleCov.start { add_filter '/test/' } if ENV['COVERAGE']
 
 require 'minitest/autorun'
