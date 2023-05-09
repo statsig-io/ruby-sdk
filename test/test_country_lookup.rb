bb0354e8427b8699c1b3411379492eba1c28dd76
Kenny Yi <kenny@statsig.com>
2023-05-08 17:58:22 -0700

    add test

---
+++ test/test_country_lookup.rb
@@ -0,0 +1,26 @@
+# typed: true
+
+require_relative 'test_helper'
+require 'minitest'
+require 'evaluator'
+
+class CountryLookupTest < Minitest::Test
+  def test_initialize_async
+    bg_thread = CountryLookup.initializeAsync
+    assert(bg_thread.alive?)
+    assert(CountryLookup.is_ready_for_lookup == false)
+    sleep 1
+    assert(!bg_thread.alive?)
+    assert(CountryLookup.is_ready_for_lookup == true)
+  end
+
+  def test_no_race_condition
+    bg_thread1 = CountryLookup.initializeAsync
+    assert(bg_thread1.alive?)
+    bg_thread2 = CountryLookup.initializeAsync
+    assert(!bg_thread1.alive?)
+    assert(bg_thread2.alive?)
+    CountryLookup.initialize
+    assert(!bg_thread2.alive?)
+  end
+end
