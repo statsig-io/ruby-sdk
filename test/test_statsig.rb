c6f578403edc6b636588722876fc2cf0ea8e944c
Kenny Yi <kenny@statsig.com>
2023-12-13 16:15:28 -0800

    API to get list of gate, config, layer names

--- test/test_statsig.rb
+++ test/test_statsig.rb
@@ -32,12 +32,12 @@ class TestStatsig < BaseTest
 
   def test_no_userid_raises
     Statsig.initialize('secret-123')
-    assert_raises{ Statsig.check_gate(StatsigUser.new({'email' => 'jkw@statsig.com'}), 'test_email')}
-    assert_raises{ Statsig.get_config(StatsigUser.new({'email' => 'jkw@statsig.com'}), 'fake_config_name')}
-    assert_raises{ Statsig.check_gate(StatsigUser.new({'email' => 'jkw@statsig.com', 'custom_ids' => {}}), 'test_email')}
-    assert_raises{ Statsig.get_config(StatsigUser.new({'email' => 'jkw@statsig.com', 'custom_ids' => {}}), 'fake_config_name')}
-    Statsig.check_gate(StatsigUser.new({'email' => 'jkw@statsig.com', 'custom_ids' => {'cid' => '1'}}), 'test_email')
-    Statsig.get_config(StatsigUser.new({'email' => 'jkw@statsig.com', 'custom_ids' => {'cid' => '1'}}), 'fake_config_name')
+    assert_raises { Statsig.check_gate(StatsigUser.new({ 'email' => 'jkw@statsig.com' }), 'test_email') }
+    assert_raises { Statsig.get_config(StatsigUser.new({ 'email' => 'jkw@statsig.com' }), 'fake_config_name') }
+    assert_raises { Statsig.check_gate(StatsigUser.new({ 'email' => 'jkw@statsig.com', 'custom_ids' => {} }), 'test_email') }
+    assert_raises { Statsig.get_config(StatsigUser.new({ 'email' => 'jkw@statsig.com', 'custom_ids' => {} }), 'fake_config_name') }
+    Statsig.check_gate(StatsigUser.new({ 'email' => 'jkw@statsig.com', 'custom_ids' => { 'cid' => '1' } }), 'test_email')
+    Statsig.get_config(StatsigUser.new({ 'email' => 'jkw@statsig.com', 'custom_ids' => { 'cid' => '1' } }), 'fake_config_name')
   end
 
   def test_error_callback_called
@@ -49,19 +49,29 @@ class TestStatsig < BaseTest
   def test_bootstrap_option
     # without bootstrap gate should evaluate to false
     Statsig.initialize('secret-123')
-    assert_equal(Statsig.check_gate(StatsigUser.new({'userID' => 'jkw'}), 'always_on_gate'), false)
+    assert_equal(Statsig.check_gate(StatsigUser.new({ 'userID' => 'jkw' }), 'always_on_gate'), false)
     Statsig.shutdown
 
     # without bootstrap gate should evaluate to true due to bootstrapped values
     options = StatsigOptions.new(bootstrap_values: @@json_file)
     Statsig.initialize('secret-123', options)
-    assert_equal(Statsig.check_gate(StatsigUser.new({'userID' => 'jkw'}), 'always_on_gate'), true)
+    assert_equal(Statsig.check_gate(StatsigUser.new({ 'userID' => 'jkw' }), 'always_on_gate'), true)
   end
 
   def test_version
     spec = Gem::Specification::load("#{__dir__}/../statsig.gemspec")
     metadata = Statsig.get_statsig_metadata
     assert_equal(spec.version.to_s, metadata["sdkVersion"])
+  end
+
+  def test_list_apis
+    options = StatsigOptions.new(bootstrap_values: @@json_file)
+    Statsig.initialize('secret-123', options)
+    assert_includes(Statsig.list_gates, 'always_on_gate')
+    assert_includes(Statsig.list_configs, 'test_config')
+    assert_includes(Statsig.list_experiments, 'sample_experiment')
+    assert_includes(Statsig.list_autotunes, 'test_autotune')
+    assert_includes(Statsig.list_layers, 'a_layer')
   end
 
   def teardown
