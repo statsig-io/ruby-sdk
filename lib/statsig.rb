5b373ab9f2555d18bcb957b2ff70a4b0ad3a931f
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2024-02-26 12:35:24 -0800

    [release] 1.33.1 - [bugfix] require set (#310)
    Fixes a bug: `uninitialized constant Statsig::Const::Set`
    
    
    
    
    
    >Included In This Release
    >- 1337afa844f659de27198d217bcf90f4a9c377cc kenny-statsig
    >   - require set (#309)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -356,7 +356,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '1.33.0',
+      'sdkVersion' => '1.33.1',
       'languageVersion' => RUBY_VERSION
     }
   end
