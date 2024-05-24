b22b86920eb14a665beb5455e25ece828f23925a
kenny-statsig <111380336+kenny-statsig@users.noreply.github.com>
2024-05-24 09:50:27 -0700

    fix 2.5.0 tests (#334)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,18 @@
 require 'simplecov'
 require 'simplecov-lcov'
 require 'simplecov-cobertura'
-SimpleCov.formatter = if ENV['COVERAGE_FORMAT'] == 'cobertura'
-                        SimpleCov::Formatter::CoberturaFormatter
-                      elsif ENV['COVERAGE_FORMAT'] == 'lcov'
-                        SimpleCov::Formatter::LcovFormatter
-                      else
-                        SimpleCov::Formatter::HTMLFormatter
-                      end
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.start { add_filter '/test/' } if ENV['COVERAGE']
+if ENV['COVERAGE'] == 'true'
+  SimpleCov.formatter = case ENV['COVERAGE_FORMAT']
+                        when 'cobertura'
+                          SimpleCov::Formatter::CoberturaFormatter
+                        when 'lcov'
+                          SimpleCov::Formatter::LcovFormatter
+                        else
+                          SimpleCov::Formatter::HTMLFormatter
+                        end
+  SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+  SimpleCov.start { add_filter '/test/' }
+end
 
 require 'minitest'
 require 'minitest/autorun'
