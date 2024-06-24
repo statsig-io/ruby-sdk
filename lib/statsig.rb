17fe8adb5bcc2529b132f93b712b0ec3dec32121
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2024-06-24 16:39:18 -0700

    [release] 1.34.2 - get_client_initialize_response + target app improvement (#340)
    ### Improvements
    - Slight perf improvement to `get_client_initialize_response` api by
    moving a target app check out of the config loop
    
    
    
    
    
    >Included In This Release
    >- cf32501ed1777d6ca13278fd20ea472f7b7b71e1 tore-statsig
    >   - fix: move target app logic out of gcir loop (#339)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -363,7 +363,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '1.34.1',
+      'sdkVersion' => '1.34.2',
       'languageVersion' => RUBY_VERSION
     }
   end
