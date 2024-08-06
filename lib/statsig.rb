e8994109c35720dba9db7c0111558db4e8b45e1b
sroyal-statsig <76536058+sroyal-statsig@users.noreply.github.com>
2024-08-06 15:20:07 -0700

    [release] 2.0.1 - Remove Private Attributes in GCIR (#347)
    ### Fixes
    - Removes private attributes from the user object being passed as part
    of the get_client_initialize_response
    
    
    
    
    
    >Included In This Release
    >- 491331c2e187a43b13a8a9bebdc72cd5fd827401 sroyal-statsig
    >   - GCIR Remove Private Attributes (#346)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -363,7 +363,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '2.0.0',
+      'sdkVersion' => '2.0.1',
       'languageVersion' => RUBY_VERSION
     }
   end
