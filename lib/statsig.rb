c12e632f36f77a915240724ad4910b84250e6336
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2024-09-26 11:26:04 -0700

    [release] 2.1.0 - Support array operators, Loosen typed numeric check, Hash 2nd exposures in GCIR, Other Improvements (#352)
    ### New Features
    - Loosen typed numeric check, The `get_typed` method on `Layer` and
    `DynamicConfig` classes now convert between Integer and Float when
    appropriate, allowing for more flexibility in handling numeric values.
      - Support array operators in evaluator
      ### Improvements
      - Improved evaluation process and enhanced 
    performance.
      - Hashed Secondary Exposures for `getClientIntializeResponse` method
    
    
    
    
    
    
    >Included In This Release
    >- bc9d221e614a3fac5056fba00440029a7201e440 Weihao Ding
    >   - optimize: eval pass percent process (#351)
    >- 9be1848fadfffa5e112e4faefeb79f89ad3c0ad6 sroyal-statsig
    >   - Hash Secondary Exposures for GCIR (#350)
    >- 9981fcaef1e5692897fba9d1b3c8788c25695a8b tore-statsig
    >   - feat: loosen typed numeric check (#349)
    >- bc3a92ada653cb41f06d3a35be75a89a3bfab4a7 sroyal-statsig
    >   - Support Array Operators (#348)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -363,7 +363,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '2.0.1',
+      'sdkVersion' => '2.1.0',
       'languageVersion' => RUBY_VERSION
     }
   end
