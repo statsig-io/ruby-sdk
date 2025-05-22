07af4c80b2f68a5932a6b38d82350a7b71ba797f
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2025-05-22 13:38:21 -0700

    [release] 2.4.3 - bugfix - symbol lookups on experiments from persistentassignment (#382)
    ### Improvements
      - slight perf improvement to djb2 hash
      ### Fixes
    - Fixes a bug where experiments fetched via PersistentAssignment wouldnt
    have symbolized keys
    
    
    
    
    
    >Included In This Release
    >- 5e85af6491308a0b998ce60c0f311cb23a5d9de5 tore-statsig
    >   - fix: symbols and string lookups in dynamic configs (#379)
    >- c1ab29b35115ef8157d9755c95c17c22cee35f41 tore-statsig
    >   - feat: optimize djb2 hash perf (#378)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -370,7 +370,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '2.4.2',
+      'sdkVersion' => '2.4.3',
       'languageVersion' => RUBY_VERSION
     }
   end
