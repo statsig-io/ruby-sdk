29ac6f973ba272a78103e7cd05aa4cd29617c326
sroyal-statsig <76536058+sroyal-statsig@users.noreply.github.com>
2025-10-14 12:08:46 -0700

    [release] 2.7.0 - Add Support for Fields Used Functions (#403)
    ### New Features
      - Adds functions that will specify fields needed to evaluate a config
    
    
    
    
    
    >Included In This Release
    >- 54d3d75d9343d7f4656491334f9166a50d5d34d6 sroyal-statsig
    >   - Support Fields Used (#401)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -406,7 +406,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '2.6.0',
+      'sdkVersion' => '2.7.0',
       'languageVersion' => RUBY_VERSION
     }
   end
