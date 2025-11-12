f7415e82a4bdaaf19a85fa5d2c5fc79c458b61b6
Logan Foster <lfoster@statsig.com>
2025-11-12 09:12:16 -0800

    [release] 2.8.1 - fix connection leak in post_logs (#412)
    ### Fixes
    -fix connection leak in post_logs
    
    
    
    
    
    >Included In This Release
    >- 2a7091f010b8f236c0b5f0930e0660c5409f1b9a Logan Foster
    >   - fix connection leak in post_logs (#410)

--- lib/statsig.rb
+++ lib/statsig.rb
@@ -410,7 +410,7 @@ module Statsig
   def self.get_statsig_metadata
     {
       'sdkType' => 'ruby-server',
-      'sdkVersion' => '2.8.0',
+      'sdkVersion' => '2.8.1',
       'languageVersion' => RUBY_VERSION
     }
   end
